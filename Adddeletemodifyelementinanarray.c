29bb87ae1604c0cdf58edac59428bb4e189d76ac
chinmaikari <101518927+Chinmai01@users.noreply.github.com>
2022-05-31 16:32:37 +0000

    outputadded

--- Adddeletemodifyelementinanarray.c
+++ Adddeletemodifyelementinanarray.c
@@ -77,3 +77,12 @@ int main()
     }
     return 0;
 }
+output:
+Enter size of array3
+Enter 1 element2
+Enter 2 element4
+Enter 3 element5
+Enter i to insert/ d to deletei
+Enter element to insert4
+Enter positon2
+2 4 4 5 ~/19ECB132-8$ 
