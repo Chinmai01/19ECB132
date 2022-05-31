1eb66d206feae64fdf7d9e6f843c84e5613cebf4
chinmaikari <101518927+Chinmai01@users.noreply.github.com>
2022-05-31 17:17:16 +0000

    output added

--- sumoftwonumbers.c
+++ sumoftwonumbers.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
-int main() {
+int main() 
+{
 int a, b, sum;
 printf("\nEnter two no: ");
 scanf("%d %d", &a, &b);
 sum = a + b;
 printf("Sum : %d", sum);
 return(0);
+}
+output:
+Enter two no: 45
+54
+Sum : 99
